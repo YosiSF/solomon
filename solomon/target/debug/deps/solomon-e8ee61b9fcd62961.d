3c68a90c4dec5cf012f5098b56e807bf66dc6016
Karl Whitford <cevb@protonmail.com>
2019-12-22 18:47:41 -0800

    Add Dependencies
    remember to get rid of all meta from intellij and the rest.

---
+++ solomon/target/debug/deps/solomon-e8ee61b9fcd62961.d
@@ -0,0 +1,5 @@
+/Users/whitfordpollard/Documents/GitHub/solomon/solomon/target/debug/deps/solomon-e8ee61b9fcd62961: src/main.rs
+
+/Users/whitfordpollard/Documents/GitHub/solomon/solomon/target/debug/deps/solomon-e8ee61b9fcd62961.d: src/main.rs
+
+src/main.rs:
---
+++ solomon/target/debug/solomon.d
@@ -0,0 +1,1 @@
+/Users/whitfordpollard/Documents/GitHub/solomon/solomon/target/debug/solomon: /Users/whitfordpollard/Documents/GitHub/solomon/solomon/src/main.rs
